985c7fc91bce0b456e08f0791fe6ef645f963a39
Debjeet Ghosh <debjeetghosh2149@gmail.com>
2024-01-23 21:45:37 +0530

    Update 1.3.3 naming convention.js

--- JAVASCRIPT/Web Learning/1.3.3 naming convention.js
+++ JAVASCRIPT/Web Learning/1.3.3 naming convention.js
@@ -3,6 +3,11 @@ let _code  = 'I love coding';
 let num = 007;
 let $123 = 100;
 
+/* the varibale name must be started with $ or _ , 
+    it must be case sensitive.
+    no number is there for the start of the variable name.
+*/
+
 console.log(bestApp);
 console.log(_code);
 console.log(num);
