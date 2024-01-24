f5718709c06f8f8aaa01750188f3c14a2e05aab5
Debjeet Ghosh <debjeetghosh2149@gmail.com>
2024-01-24 23:39:46 +0530

    Create string concatanation.js

---
+++ JAVASCRIPT/Web Learning/operators/string concatanation.js
@@ -0,0 +1,8 @@
+let str1 = 'ambush';
+let str2 = 101;
+let str3 = false;
+
+let str4=str2+' '+str3+str1;      // string concatanation
+console.log(typeof str4);  
+
+console.log(str2+str1);   // another wy to string concatanate
