3cf9cd7454a9be8cd7c66338e8290abfc4ff8a0f
Debjeet Ghosh <debjeetghosh2149@gmail.com>
2024-01-24 23:08:42 +0530

    Create typeof operator.js

---
+++ JAVASCRIPT/Web Learning/operators/typeof operator.js
@@ -0,0 +1,10 @@
+// typeof is used to find the type of the data used here
+let boolVar = true;
+let StrVar = 'asgard';
+let numVar = 1001;
+let Know;
+
+console.log(typeof Know);
+console.log(typeof boolVar);
+console.log(typeof StrVar);
+console.log(typeof numVar);
