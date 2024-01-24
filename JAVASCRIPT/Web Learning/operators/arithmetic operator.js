8e48bc1110d75db746ce73a8873b255d7797cd21
Debjeet Ghosh <debjeetghosh2149@gmail.com>
2024-01-24 12:08:31 +0530

    Create arithmetic operator.js

---
+++ JAVASCRIPT/Web Learning/operators/arithmetic operator.js
@@ -0,0 +1,21 @@
+let x = 5;
+let y = 2;
+var z = x+y;
+var subtract = x-y;
+console.log(z);
+console.log(subtract);
+
+let a = 200;
+let b = 21;
+var c = a/b;
+var mult = a*b;
+var module = a%b;
+console.log(c);
+console.log(mult);
+console.log(module);
+
+let p = 12;
+p++;
+console.log(p);
+p--;
+console.log(p);
