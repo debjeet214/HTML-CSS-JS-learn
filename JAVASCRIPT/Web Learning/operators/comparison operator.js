5425090f331395c6ef8c993782fee704fc375a52
Debjeet Ghosh <debjeetghosh2149@gmail.com>
2024-01-24 12:26:54 +0530

    Update comparison operator.js

--- JAVASCRIPT/Web Learning/operators/comparison operator.js
+++ JAVASCRIPT/Web Learning/operators/comparison operator.js
@@ -5,6 +5,6 @@ let z = "10";
 var xy = x>=y;
 console.log(xy);
 
-console.log(x === z);  // strict equal to
-console.log(x!==y);    //strict not equal to
-console.log(x==y);     // equal to only
+console.log(x === z);  // strict equal to, it returns false even if the value is same but if the data type is different.
+console.log(x!==y);    //strict not equal to, it returns true if any one thing doesn't match  
+console.log(x==y);     // equal to only, it can be true if the value is equal even the data type is different.
