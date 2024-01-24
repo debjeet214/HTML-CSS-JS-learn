d8f71cda3f462bdb67eb043309844a8faf3d896f
Debjeet Ghosh <debjeetghosh2149@gmail.com>
2024-01-24 12:25:26 +0530

    Create comparison operator.js

---
+++ JAVASCRIPT/Web Learning/operators/comparison operator.js
@@ -0,0 +1,10 @@
+let x = 10;
+let y = 50;
+let z = "10";
+
+var xy = x>=y;
+console.log(xy);
+
+console.log(x === z);  // strict equal to
+console.log(x!==y);    //strict not equal to
+console.log(x==y);     // equal to only
