87881eac228292fb16ec4de94a52abc2e7ca2925
Debjeet Ghosh <debjeetghosh2149@gmail.com>
2024-01-24 23:59:34 +0530

    Update 1.4.1 conditional statement.js

--- JAVASCRIPT/Web Learning/1.4.1 conditional statement.js
+++ JAVASCRIPT/Web Learning/1.4.1 conditional statement.js
@@ -1,4 +1,10 @@
-let marks = 35;
-if(marks >= 33){
-    console.log("Examination Passed");
+let marks = 30;
+// add logic here
+
+if(marks >= 33)
+{
+	console.log("Examination Passed");
+}
+else{
+    console.log("Examination Failed");
 }
