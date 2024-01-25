96dd0047b211db6a61b2ebfcab749460ceaef780
Debjeet Ghosh <debjeetghosh2149@gmail.com>
2024-01-25 13:06:40 +0530

    Create stop after printing 5.js

---
+++ JAVASCRIPT/Good Code/stop after printing 5.js
@@ -0,0 +1,20 @@
+let x = 0;
+
+//way 1
+while(x < 10){
+        console.log(x);
+        if(x === 5){
+            break;
+        }
+        x++;
+}
+
+//way 2
+while(x < 10){
+        if(x > 5){
+            break;
+        }
+  else
+      console.log(x);
+      x++;
+}
