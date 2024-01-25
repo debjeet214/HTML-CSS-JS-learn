7fb6da7f723d86f49830f11b9a7421d0295e6bd8
Debjeet Ghosh <debjeetghosh2149@gmail.com>
2024-01-25 23:21:48 +0530

    Create dividing through function.js

---
+++ JAVASCRIPT/Web Learning/function/dividing through function.js
@@ -0,0 +1,13 @@
+function division(x,y)
+{
+	let result = x/y;
+	if(y != 0) {
+
+		console.log("Result: "+ result);
+	}
+	else {
+		console.log("Cannot divide a number by zero");
+	} 
+}
+// calling the function
+division(10, 5);
