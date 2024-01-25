f86113c9804b8c123865c2e39ff09f7b8f7380a7
Debjeet Ghosh <debjeetghosh2149@gmail.com>
2024-01-25 19:48:20 +0530

    Update and rename 1.7 array.js to 1.7 array basic.js

---
+++ JAVASCRIPT/Web Learning/1.7 array basic.js
@@ -0,0 +1,12 @@
+let subjects = ["HTML", "CSS", "JavaScript", "Php", "Bootstrap"];
+
+console.log(subjects[0].length);
+
+
+// in javscript araay can hold all types of data types together.
+var arr = ["Apple", 23, "Mango", 2.4, "a"];
+
+var counting;
+for(counting =0; counting<5; counting++){
+    console.log(arr[counting]);
+}
--- JAVASCRIPT/Web Learning/1.7 array.js
+++
@@ -1,3 +0,0 @@
-let subjects = ["HTML", "CSS", "JavaScript", "Php", "Bootstrap"];
-
-console.log(subjects[0].length);
