dde25d3410eb0e00ab3d11fb3691da69f0935954
Debjeet Ghosh <debjeetghosh2149@gmail.com>
2024-01-25 19:28:07 +0530

    Create 1.6.2 string output.js

---
+++ JAVASCRIPT/Web Learning/1.6.2 string output.js
@@ -0,0 +1,8 @@
+let str1 = 'CE';
+let str2 = '2253';
+let str3 = '2K7';
+
+// join string using +
+console.log(str1+'/'+str2+'/'+str3);
+// use template literal
+console.log(`${str1}/${str2}/${str3}`);
