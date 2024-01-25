4fc5439a91b25354bd2fa68cb3b2e45a5850c48e
Debjeet Ghosh <debjeetghosh2149@gmail.com>
2024-01-25 19:23:04 +0530

    Create 1.6 string declaration ways.js

---
+++ JAVASCRIPT/Web Learning/1.6 string declaration ways.js
@@ -0,0 +1,17 @@
+// different ways to declear and initialize the string
+
+var str = " we can use double quote for string";
+var str3 = 'we can use single qoute for string';
+let str1=`Studytonight using backtik`;
+var str2 = String(str1);  // using the string constructor
+var stringNew = new String("Debjeet Ghosh");
+// write code here
+
+console.log(str1);
+console.log(str2);
+console.log(str);
+console.log(str3);
+console.log(stringNew);
+
+const txtObj = new String("This is a String object");
+console.log(typeof(txtObj));
