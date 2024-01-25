8a8b33de2ad1baf6057036787b4cef150a39e271
Debjeet Ghosh <debjeetghosh2149@gmail.com>
2024-01-25 23:05:07 +0530

    Create 1.7.6 multidimensional array.js

---
+++ JAVASCRIPT/Web Learning/1.7.6 multidimensional array.js
@@ -0,0 +1,8 @@
+var fruit1 = ["Mango", 100];
+var fruit2 = ["Apple", 50];
+var fruitList = [fruit1, fruit2];
+// log the fruitList array on console
+console.log(fruitList);
+
+//another way
+const record = [[1, 2, 3], [5, 7, 4], [4, 5, 6]];//multidimensional array
