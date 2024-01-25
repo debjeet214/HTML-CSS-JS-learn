11ab3e2fcec040e217833baea3e3fa7ac3862e82
Debjeet Ghosh <debjeetghosh2149@gmail.com>
2024-01-25 12:08:16 +0530

    Create 1.4.3 nested if else.js

---
+++ JAVASCRIPT/Web Learning/1.4.3 nested if else.js
@@ -0,0 +1,19 @@
+let myvehicle = "car";
+let weather = "rainy";
+
+if(weather == "rainy")
+{
+	if(myvehicle == "car")
+	{
+		console.log("I go to the office");
+	}
+	else if(myvehicle == "bike") 
+	{
+		console.log("I Work from home");
+	}
+
+}
+else if(weather != "rainy")
+{
+	console.log("I go to the office");
+}
