deaec2f551ac8cc114e47b444be13c09d793b2c2
Debjeet Ghosh <debjeetghosh2149@gmail.com>
2024-01-25 19:56:52 +0530

    Create 1.7.3 update array element.js

---
+++ JAVASCRIPT/Web Learning/1.7.3 update array element.js
@@ -0,0 +1,9 @@
+let cars=["Lexus", "Jeep", "Audi", "BMW"];
+
+cars[0] = cars[2];
+console.log(cars[0]);
+
+
+let fruits = ["Apple", "Banana", "Mango", "Cherry"];
+fruits[3] = "Grapes";
+console.log(fruits);
