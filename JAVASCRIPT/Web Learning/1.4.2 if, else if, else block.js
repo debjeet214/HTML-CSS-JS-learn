75d3ca54beaedfe28fede9fc995806323d575213
Debjeet Ghosh <debjeetghosh2149@gmail.com>
2024-01-25 12:02:42 +0530

    Create 1.4.2 if, else if, else block.js

---
+++ JAVASCRIPT/Web Learning/1.4.2 if, else if, else block.js
@@ -0,0 +1,16 @@
+let signal = "yellow";
+
+if(signal == "green"){
+    console.log("Go");
+}
+else if(signal == "yellow"){
+    console.log("Wait");
+}
+
+else if(signal == "red"){
+    console.log("Stop");
+}
+
+else{
+    console.log("Error");
+}
