c5f2be869fb6efd7fe35b56e3a73a47c19d02f1c
Debjeet Ghosh <debjeetghosh2149@gmail.com>
2024-01-26 17:00:24 +0530

    Create 1.1 object declare ways.js

---
+++ JAVASCRIPT/Web Learning/objects/1.1 object declare ways.js
@@ -0,0 +1,14 @@
+let student = {name: "John", course: "DCSE", fees: 135000};
+console.log(student);
+
+
+// we can write it like,
+let studentway ={
+    name: "John",
+    course: "DCSE",
+    fees: 135000
+}
+
+
+// EQUIVALENT ARRAY IS,
+    let studentarr = ["John", "DCSE", 135000];
