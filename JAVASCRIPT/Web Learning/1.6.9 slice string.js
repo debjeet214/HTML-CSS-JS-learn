eb69a6bda9652bfac97f9ddd775b40542d74f4c8
Debjeet Ghosh <debjeetghosh2149@gmail.com>
2024-01-26 23:53:11 +0530

    Create 1.6.9 slice string.js

---
+++ JAVASCRIPT/Web Learning/1.6.9 slice string.js
@@ -0,0 +1,5 @@
+// If you want to cut out or slice out a part of the given string, then you can do so using the slice() method.
+
+let a = "Orange Banana Mango";
+let subA = a.slice(7,14);   //result of subA will be Banana 
+console.log(subA)  // it will output after 7th to 14th no. string index
