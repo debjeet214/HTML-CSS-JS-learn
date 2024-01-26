4ca83a15e9482f31a975f80981b98b5422046eed
Debjeet Ghosh <debjeetghosh2149@gmail.com>
2024-01-26 17:42:36 +0530

    Create 5. update the object property.js

---
+++ JAVASCRIPT/Web Learning/objects/5. update the object property.js
@@ -0,0 +1,10 @@
+let mobile = {
+	brand: "Apple",
+	model : "iPhone 12",
+	colors: "black"
+};
+
+var colors =["Blue", "Green", "Black", "White", "Purple"];    
+mobile.colors = colors;    // we can assign an array as the property value as multiple options
+
+console.log(mobile);
