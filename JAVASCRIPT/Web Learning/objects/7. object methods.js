8c43617b7215e1e2d1f984f0cf4a84d183071bdd
Debjeet Ghosh <debjeetghosh2149@gmail.com>
2024-01-26 18:03:37 +0530

    Update 7. object methods.js

--- JAVASCRIPT/Web Learning/objects/7. object methods.js
+++ JAVASCRIPT/Web Learning/objects/7. object methods.js
@@ -9,3 +9,15 @@ let mobile = {
 };
 
 mobile.purchase(2);
+
+// Another Example. 
+let car = {
+    brand: "Bugatti",
+    model: "Chiron-Sports",
+    color: "Blackish-Violet",
+    price: 4000000,
+    speed: 483
+}
+car.details= new function(){
+    console.log(`The car i am going to buy is ${this.brand} ${this.model}, whose price is ${this.price} dollars and it's top speed is ${this.speed}`);
+}
