79ce6d2b7a61e6460fdb77e6c1985e15177c9b8d
Debjeet Ghosh <debjeetghosh2149@gmail.com>
2024-01-26 17:30:54 +0530

    Create 3. add new property from outside.js

---
+++ JAVASCRIPT/Web Learning/objects/3. add new property from outside.js
@@ -0,0 +1,5 @@
+let holidayPlan = {venue:"Maldives", duration: "4 days", cost: 150000, person: 2};
+// add more info using dot operator
+holidayPlan.flightIncluded = false;
+// add more info using square bracket
+holidayPlan['sightSeeing'] = true;
