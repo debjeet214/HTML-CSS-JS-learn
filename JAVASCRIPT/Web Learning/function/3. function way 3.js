4ae6546238bb678e6d77c43b9d309c153896af91
Debjeet Ghosh <debjeetghosh2149@gmail.com>
2024-01-26 16:36:26 +0530

    Create 3. function way 3.js

---
+++ JAVASCRIPT/Web Learning/function/3. function way 3.js
@@ -0,0 +1,13 @@
+// for functions with parenthesis
+let welcome = (a,b) => console.log(a+b);
+welcome("Hello ", "Studytonight");
+
+
+// for function without the parameters,
+let welcome = () => console.log("Hello! and Welcome.");
+welcome();   // function call
+
+
+// for functions with one parenthesis, we can omit brackets
+let welcome = name => console.log(`Welcome ${x}`);
+welcome("John");
