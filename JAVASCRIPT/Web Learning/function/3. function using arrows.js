a6adf5cac98cc7ee36bd6227b643da34fe0f1bfd
Debjeet Ghosh <debjeetghosh2149@gmail.com>
2024-01-28 17:47:24 +0530

    Update 3. function using arrows.js

--- JAVASCRIPT/Web Learning/function/3. function using arrows.js
+++ JAVASCRIPT/Web Learning/function/3. function using arrows.js
@@ -1,4 +1,4 @@
-// for functions with parenthesis
+// for functions with multiple parameters
 let welcome = (a,b) => console.log(a+b);
 welcome("Hello ", "Studytonight");
 
@@ -8,6 +8,17 @@ let welcome = () => console.log("Hello! and Welcome.");
 welcome();   // function call
 
 
-// for functions with one parenthesis, we can omit brackets
-let welcome = name => console.log(`Welcome ${x}`);
+// for functions with one parameters, we can omit brackets
+let welcome = name =>{
+  console.log(`Welcome ${x}`);
+}
 welcome("John");
+ 
+//or we can do this way
+
+let func1 = (age) =>{
+  console.log("the age is: ", age); 
+  console.log("i will be "+ (age+1) +" soon")
+}
+func1(19);
+func1(23);
