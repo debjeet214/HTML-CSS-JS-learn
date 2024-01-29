474a26f936832a141db4c3ad25d7f68eb6ae875a
Debjeet Ghosh <debjeetghosh2149@gmail.com>
2024-01-29 11:15:18 +0530

    Update and rename 1.7.1 array functions.html to 1.7 array functions.js

---
+++ JAVASCRIPT/Web Learning/Array/1.7 array functions.js
@@ -0,0 +1,13 @@
+
+let arr1 = [1, 2, 3];
+let arr2 = [4, 5, 6];
+console.log(arr1.concat(arr2));
+
+let numbers = [1, 29, 22, 88, 21, 54, 33, 44];  
+
+// perform operations
+numbers.sort();                     	// to sort array elements
+numbers.reverse().pop();  		// to pop last array element
+numbers.reverse().push(55555);  	// to push at the end
+document.write(numbers);
+
