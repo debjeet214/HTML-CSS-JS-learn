07db0c7493a9cd46dc31d31ee419dcf392e02727
Debjeet Ghosh <debjeetghosh2149@gmail.com>
2024-01-29 22:39:52 +0530

    Create Factorial.js

---
+++ JAVASCRIPT/Practice code/Factorial.js
@@ -0,0 +1,20 @@
+// using recursion function
+function factorial(n) { 
+    if (n === 0 || n === 1) { 
+        return 1; 
+    } else { 
+        return n * factorial(n - 1); 
+    } 
+} 
+let num1 = prompt("enter the number: "); 
+let result = factorial(num1); 
+console.log("The factorial of given number is :" + result);
+
+
+// using for loop
+let x;
+let y =1;
+for( x =1; x<=num1;x++){
+    y = y*x;
+}
+console.log(y);
