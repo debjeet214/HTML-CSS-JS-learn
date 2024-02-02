765c0b778a6a187fe50af685a86be6a0d84b21fb
Debjeet Ghosh <debjeetghosh2149@gmail.com>
2024-02-02 22:55:15 +0530

    Create 2. simple example.js

---
+++ JAVASCRIPT/WebDev/3. Async & Await/2. simple example.js
@@ -0,0 +1,18 @@
+let num = prompt("enter the correct pass number")
+
+async function getData(){
+    return new Promise((resolve, reject)=>{
+        setTimeout(()=>{
+            resolve(21)
+        }, 1000)
+    })
+}
+
+async function check(){
+    console.log('Hey, you are inside the async function');
+    const result = await getData()
+    console.log(result)
+    console.log("all done, bye bye")
+}
+
+check()
