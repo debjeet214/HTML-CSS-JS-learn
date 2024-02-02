cb06042877509942b597cc193e37b82379816da0
Debjeet Ghosh <debjeetghosh2149@gmail.com>
2024-02-02 23:28:03 +0530

    Create 4. waiting for a file.js

---
+++ JAVASCRIPT/WebDev/3. Async & Await/4. waiting for a file.js
@@ -0,0 +1,17 @@
+async function getFile() {
+  let myPromise = new Promise(function(resolve) {
+    let req = new XMLHttpRequest();
+    req.open('GET', "mycar.html");
+    req.onload = function() {
+      if (req.status == 200) {
+        resolve(req.response);
+      } else {
+        resolve("File not Found");
+      }
+    };
+    req.send();
+  });
+  document.getElementById("demo").innerHTML = await myPromise;
+}
+
+getFile();
