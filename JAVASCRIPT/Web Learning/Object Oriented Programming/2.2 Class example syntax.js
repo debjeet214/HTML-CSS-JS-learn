530cef8119f28f2098e4f3b76b498a9fc19dfe2a
Debjeet Ghosh <debjeetghosh2149@gmail.com>
2024-02-04 00:55:39 +0530

    Create 2.2 Class example syntax.js

---
+++ JAVASCRIPT/Web Learning/Object Oriented Programming/2.2 Class example syntax.js
@@ -0,0 +1,18 @@
+// Defining class using es6
+class Vehicle {
+	constructor(name, maker, engine) {
+		this.name = name;
+		this.maker = maker;
+		this.engine = engine;
+	}
+	getDetails() {
+		return (`The name of the bike is ${this.name}.`)
+	}
+}
+// Making object with the help of the constructor
+let bike1 = new Vehicle('Hayabusa', 'Suzuki', '1340cc');
+let bike2 = new Vehicle('Ninja', 'Kawasaki', '998cc');
+
+console.log(bike1.name); // Hayabusa
+console.log(bike2.maker); // Kawasaki
+console.log(bike1.getDetails());
