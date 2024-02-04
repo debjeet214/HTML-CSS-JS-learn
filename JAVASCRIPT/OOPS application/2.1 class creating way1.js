1668154046af142056ca1c2cfeaa884d2df9245e
Debjeet Ghosh <debjeetghosh2149@gmail.com>
2024-02-04 23:00:18 +0530

    Create 2.1 class creating way1.js

---
+++ JAVASCRIPT/OOPS application/2.1 class creating way1.js
@@ -0,0 +1,25 @@
+class BankAccount{   // class declaration
+    customerName;
+    accountNumber = Date.now();
+    balance = 0;
+
+    constructor(customerName, balance = 0){
+        this.customerName = customerName;    // properties of the class bankAccount
+        this.accountNumber = Date.now();     //  to generate a random account number
+        this.balance = balance;    
+    }
+
+    deposit(amount){
+        this.balance += amount;
+    }
+    withdrawl(amount){
+        this.balance -= amount;
+    }
+}
+
+const Account1 = new BankAccount('debjeet ghosh', 34200);
+console.log(Account1);
+Account1.deposit(1200);
+
+const Account2 = new BankAccount('Sneha Ghosh', 1000);
+console.log(Account2);
