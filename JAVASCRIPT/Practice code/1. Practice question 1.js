2011687281962ca87571db0cf30275be9f9cec12
Debjeet Ghosh <debjeetghosh2149@gmail.com>
2024-02-05 23:20:10 +0530

    Update Practice question 1.js

--- JAVASCRIPT/Practice code/1. Practice question 1.js
+++ JAVASCRIPT/Practice code/1. Practice question 1.js
@@ -4,7 +4,7 @@ Implement a JavaScript function that takes an array of student names and assigns
 or Slytherin(length greater than or equal to 12)) based on the length of their names.*/
 
 
-
+//   WAY - 1  ( USER INPUT  DINAMICALLY )
 const students = [];
 let count = prompt("enter how many students you have in the game today: ")
 for(let x = 1; x<= count; x++){
@@ -26,3 +26,26 @@ for(let x = 1; x<= count; x++){
 }
 
 console.log(students);
+
+
+// WAY - 2     ( GIVEN ARRAY )
+let students = ["shubh", "anajali", "Shivani", "Shivangi", "Annapurna", "Shubham", "Krishnanendu", "Ravindranathan", "Shivesh", "kaif", "Emanuel"]
+
+let houses = []
+
+for (const student of students) {
+    if(student.length < 6){
+        houses.push("Gryffindor")
+    }
+    else if(student.length < 8){
+        houses.push("Hufflepuff")
+    }
+    else if(student.length < 12){
+        houses.push("Ravenclaw")
+    }
+    else{
+        houses.push("Slytherin")
+    }
+}
+
+console.log(houses)
