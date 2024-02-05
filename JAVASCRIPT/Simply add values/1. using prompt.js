511e581cb79eefe05278dc37a0365d9c7a7963cf
Debjeet Ghosh <debjeetghosh2149@gmail.com>
2024-02-05 21:18:35 +0530

    Create 1. using prompt.js

---
+++ JAVASCRIPT/Simply add values/1. using prompt.js
@@ -0,0 +1,8 @@
+let a = prompt("Enter first number: ")
+let b = prompt("Enter second number: ")
+
+let stringaddition = a+b;   // this will just append the two string into result
+let sum = parseInt(a) + parseInt(b)  // this will first convert the input num-string into integer & the add
+
+console.log("The sum is" , sum);
+console.log("Appended form is ", stringaddition)
