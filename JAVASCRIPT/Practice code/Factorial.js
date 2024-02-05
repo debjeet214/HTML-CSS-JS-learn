79a1e9b5ef3e5a173b731f8fd55def18f0abc953
Debjeet Ghosh <debjeetghosh2149@gmail.com>
2024-02-05 23:21:07 +0530

    Update Factorial.js

--- JAVASCRIPT/Practice code/Factorial.js
+++ JAVASCRIPT/Practice code/Factorial.js
@@ -1,4 +1,4 @@
-// using recursion function
+// way - 1 (using recursion function)
 function factorial(n) { 
     if (n === 0 || n === 1) { 
         return 1; 
@@ -11,7 +11,7 @@ let result = factorial(num1);
 console.log("The factorial of given number is :" + result);
 
 
-// using for loop
+// way 2 ( using for loop ) 
 let x;
 let y =1;
 for( x =1; x<=num1;x++){
