3e2fe5a0b77a85eeed3604727e262a8ede4a944a
Debjeet Ghosh <debjeetghosh2149@gmail.com>
2024-02-05 20:25:01 +0530

    Create 5. Immediately invoke function..js

---
+++ JAVASCRIPT/WebDev/3. Async & Await/5. Immediately invoke function..js
@@ -0,0 +1,14 @@
+async function sleep(){
+    return new Promise((resolve, reject)=>{
+        setTimeout(()=>{
+            resolve(45)
+        }, 1000);
+    })
+}
+
+( async function main(){
+    let a = await sleep()
+    console.log(a)
+    let b = await sleep()
+    console.log(b)
+})()
