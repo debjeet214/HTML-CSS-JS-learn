b80237c8d85b1f7f452ae72a59258c4dd88ac320
Debjeet Ghosh <debjeetghosh2149@gmail.com>
2024-11-15 17:15:05 +0530

    Create factorial.js

---
+++ JAVASCRIPT/day-1/factorial.js
@@ -0,0 +1,12 @@
+//  factorial of value
+
+n = 7
+function factorial(n) {
+    if (n == 0) {
+        return 1;
+    } else {
+        return n * factorial(n-1);
+    }
+}
+
+console.log(factorial(n))
